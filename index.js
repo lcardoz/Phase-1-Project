71d8efefaacd669c5f3b25beea44ed7b93d09fe5
Leah Cardoz <leahcardoz@gmail.com>
2022-10-25 15:35:12 -0400

    got all deliverables

--- index.js
+++ index.js
@@ -1,70 +1,71 @@
-// console.log('hello')
-const docBody = document.querySelector('body');
-const searchContainer = document.querySelector('form');
-const imgContainer = document.createElement('div');
-const imgNasa = document.createElement('img');
-const imgTitle = document.createElement('h1');
-const imgDescription = document.createElement('p');
-
 document.addEventListener('DOMContentLoaded', () => {
+    const searchContainer = document.querySelector('#search-form');
+    const imgNasa = document.querySelector('#nasa-image');
+    const imgTitle = document.querySelector('#image-title');
+    const imgDescription = document.querySelector('#image-description');
 
+    let resultArray = [];
+    let currentIndex = 0;
+
+    let renderSearch = (spaceObject) => {
+        // console.log(spaceObject);
+       
+        imgTitle.innerText = spaceObject.data[0].title
+        imgDescription.innerText = spaceObject.data[0].description
+        
+        if (spaceObject.links !== undefined) {
+            imgNasa.src = spaceObject.links[0].href
+        }
+        else {
+            imgNasa.src = 'https://www.nasa.gov/sites/default/files/thumbnails/image/s75-31690.jpeg'
+        }
+    }
+
+    let renderPrevItem = () =>{
+      currentIndex -= 1
+      renderSearch(resultArray[currentIndex])
+    }
+
+    let renderNextItem = () =>{
+      currentIndex += 1
+      renderSearch(resultArray[currentIndex])
+    }
+
+    document.querySelector("#nextButton").addEventListener("click",renderNextItem)
+    document.getElementById('prevButton').addEventListener('click', renderPrevItem)
+   
     searchContainer.addEventListener('submit', (e) => {
         e.preventDefault();
         const searchInput = e.target.search.value;
-        //console.log(searchInput)
-
+        
         fetch(`https://images-api.nasa.gov/search?q=${searchInput}`)
             .then(response => response.json())
-            .then(dataObj => { 
-                //console.log(dataObj.collection)
+            .then(dataObj => {
+            //console.log(dataObj.collection.items)
+            resultArray = dataObj.collection.items;
+            //console.log ( "resultArray: " , resultArray)
+            renderSearch(resultArray[currentIndex])
+
+            const newImgDataArray = resultArray.map( (dataObj) => { 
+                return dataObj.links } )
+                //console.log( "newImgDataArray: " , newImgDataArray )
+
+            const filteredArray = newImgDataArray.filter(element => {
+                return element !== undefined;})
+                //console.log( "filteredArray: " , filteredArray )
+
+            const finalImageArray = filteredArray.map(element => {
+                return element[0].href });
+                //console.log( "finalImageArray: ", finalImageArray )
             
-                imgNasa.src = dataObj.collection.items[0].links[0].href;
-                //console.log(dataObj.collection.items[0].links[0].href);
-                imgTitle.textContent = dataObj.collection.items[0].data[0].title;
-                imgDescription.textContent = dataObj.collection.items[0].data[0].description;
-                imgContainer.appendChild(imgNasa);
-                docBody.append(searchContainer, imgTitle, imgContainer, imgDescription);
+            const galleryContainer = document.querySelector('#gallery')
 
-                const imgDataArray = dataObj.collection.items
-                console.log ( "imgDataArray: " , imgDataArray)
-                
-                const newImgDataArray = imgDataArray.map( (dataObj) => { 
-                    return dataObj.links } )
-                    console.log( "newImgDataArray: " , newImgDataArray )
-
-                const filteredArray = newImgDataArray.filter(element => {
-                    return element !== undefined;})
-                    console.log( "filteredArray: " , filteredArray )
-
-                const finalImageArray = filteredArray.map(element => {
-                    return element[0].href });
-                    console.log( "finalImageArray: ", finalImageArray )
-                
-
-                
-
-                // let newImgDataArray = [];
-                // for (let i = 0; i < dataObj.collection.items.length; i++) {
-                //     newImgDataArray.push(dataObj.collection.items[i].links[0].href)
-                // }
-                // console.log(newImgDataArray)
-                // const newArray = dataObj.collection.items.map 
-                // const imgArray = [];
-                // let itemIndex = 0;
-                // imgArray.forEach(photo =>{
-                // let photo = dataObj.collection.items[Number(itemIndex)].links[0].href
-                // imgArray.push(photo);
-                // itemIndex++;
-                // console.log(imgArray)
-})
-
-                // imgNasa.addEventListener('click', () => {
-                //     dataObj.collection.items.forEach (item => {
-                //         imgNasa.src = item.links[0].href;
-                //         console.log(item.links[0].href);
-                // })})
-            
+            finalImageArray.forEach (imageUrl => {
+                //console.log(imageUrl)
+                const galleryImage = document.createElement('img');
+                galleryImage.src = imageUrl;
+                galleryContainer.append(galleryImage)
+            })
         })
     })
-
-
+});
